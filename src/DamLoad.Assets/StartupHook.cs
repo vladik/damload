a0eb3708ddae8ceddfe733f8d333d462290e52cd
vladik <vladik@gmail.com>
2025-03-23 10:08:53 -0700

    Assets module refactoring

--- src/DamLoad.Assets/StartupHook.cs
+++ src/DamLoad.Assets/StartupHook.cs
@@ -2,11 +2,6 @@
 using DamLoad.Abstractions.Models;
 using DamLoad.Abstractions.Startup;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DamLoad.Assets
 {
