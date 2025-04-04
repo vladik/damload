9e6f102b5b72664fb81d2a89aa4eb0784d914d0c
vladik <vladik@gmail.com>
2025-04-03 18:00:31 -0700

    adding folders endpoints

---
+++ src/DamLoad.Assets/Api/V1/Folders/Endpoints/ListFolders.cs
@@ -0,0 +1,32 @@
+﻿using DamLoad.Assets.Api.V1.Folders.Responses;
+using DamLoad.Assets.Services;
+using FastEndpoints;
+
+namespace DamLoad.Assets.Api.V1.Folders.Endpoints;
+
+public class ListFolders : EndpointWithoutRequest<List<FolderResponse>>
+{
+    private readonly IFolderService _folderService;
+
+    public ListFolders(IFolderService folderService) => _folderService = folderService;
+
+    public override void Configure()
+    {
+        Get("/api/v1/folders");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var folders = await _folderService.GetAllAsync();
+        var response = folders.Select(f => new FolderResponse
+        {
+            Id = f.Id,
+            Name = f.Name,
+            ParentId = f.ParentId,
+            SortOrder = f.SortOrder
+        }).ToList();
+
+        await SendAsync(response, cancellation: ct);
+    }
+}
---
+++ src/DamLoad.Assets/Api/V1/Folders/Validators/CreateFolderValidator.cs
@@ -0,0 +1,20 @@
+﻿using DamLoad.Assets.Api.V1.Folders.Requests;
+using FastEndpoints;
+using FluentValidation;
+
+namespace DamLoad.Assets.Api.V1.Folders.Validators;
+
+public class CreateFolderValidator : Validator<CreateFolderRequest>
+{
+    public CreateFolderValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Folder name is required.")
+            .MaximumLength(100);
+
+        RuleFor(x => x.SortOrder)
+            .GreaterThanOrEqualTo(0)
+            .WithMessage("Sort order must be 0 or greater.");
+    }
+}
